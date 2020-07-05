0a139e6c37eeaf042b35feca7fa8e83bc07f8af5
Shadman Aziz <97sdmn@gmail.com>
2020-07-05 00:18:54 -0400

    added all files and images

---
+++ Dice game/index.js
@@ -0,0 +1,46 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var image1 = "images/dice"+randomNumber1+".png";
+document.getElementsByClassName("img1")[0].setAttribute("src", image1);
+
+randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var image2 = "images/dice"+randomNumber2+".png";
+document.getElementsByClassName("img2")[0].setAttribute("src", image2);
+
+// if (randomNumber1 === 1) {
+//   document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice1.png");
+// } else if (randomNumber1 === 2) {
+//   document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice2.png");
+// } else if (randomNumber1 === 3) {
+//   document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice3.png");
+// } else if (randomNumber1 === 4) {
+//   document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice4.png");
+// } else if (randomNumber1 === 5) {
+//   document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice5.png");
+// } else if (randomNumber1 === 6) {
+//   document.getElementsByClassName("img1")[0].setAttribute("src", "images/dice6.png");
+// }
+//
+//
+//
+// if (randomNumber2 === 1) {
+//   document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice1.png");
+// } else if (randomNumber2 === 2) {
+//   document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice2.png");
+// } else if (randomNumber2 === 3) {
+//   document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice3.png");
+// } else if (randomNumber2 === 4) {
+//   document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice4.png");
+// } else if (randomNumber2 === 5) {
+//   document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice5.png");
+// } else if (randomNumber2 === 6) {
+//   document.getElementsByClassName("img2")[0].setAttribute("src", "images/dice6.png");
+// }
+
+var heading = document.querySelectorAll("h1")[0];
+if (randomNumber1 > randomNumber2) {
+  heading.innerHTML="Player 1 wins!";
+} else if (randomNumber2 > randomNumber1) {
+  heading.innerHTML="Player 2 wins!";
+} else {
+  heading.innerHTML="Draw...";
+}
